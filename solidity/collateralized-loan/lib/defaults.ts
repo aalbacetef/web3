20c3d6bdb8b833079341e61f504213273cacf29d
Arturo Albacete <aalbacetef@gmail.com>
2025-04-07 00:36:03 +0200

    update defaults to include minInterestRate and maxLoanDurationInDays

--- solidity/collateralized-loan/lib/defaults.ts
+++ solidity/collateralized-loan/lib/defaults.ts
@@ -3,3 +3,7 @@ export const liquidationThreshold = 85; // percentage
 export const minimumLoanAmount = { amount: 200, currency: 'USDT' };
 export const loanRequestFeePercentage = 1;
 export const settlementFeePercentage = 1;
+export const maxLoanDurationInDays = 180;
+
+// is multiplied by 10: 35 => 3.5%
+export const minInterestRate = 35; // 3.5
