4d6a67b70d3778dd3bcd55dca8bc3839ae495a22
Arturo Albacete <aalbacetef@gmail.com>
2025-04-07 00:35:20 +0200

    add a mock token contract

---
+++ solidity/collateralized-loan/contracts/MockToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply)
+        ERC20(name, symbol)
+    {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address target, uint256 amount) external {
+      _mint(target, amount);
+    }
+}
